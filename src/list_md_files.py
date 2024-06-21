36dc27f2b24d882f2f45b48952945983893ae2fd
Tao Dong <tomdong@gmail.com>
2024-06-21 10:13:53 +0000

    Tweak report link titles

--- src/list_md_files.py
+++ src/list_md_files.py
@@ -2,18 +2,23 @@ import os
 import yaml
 import frontmatter
 
+# Format link title as "Report YYYY-MM-DD"
+def formatTitle(file_path):
+    parts = file_path[8:-3].split('_')
+    return ' '.join(word.capitalize() for word in parts)    
+
 markdown_files = []
 for root, _, files in os.walk("reports"):
     for file in files:
-        if file.endswith(".md"):
+        if file.endswith(".md") and not(file.startswith("index")):
             file_path = os.path.join(root, file)
-            post = frontmatter.load(file_path)
+            # post = frontmatter.load(file_path)
             markdown_files.append({
-                "title": post.get("title", file),
+                "title": formatTitle(file_path),
                 "url": file_path[8:-3],  
-                # Remove the leading "reports/" and the trailing ".md"
-                "date": post.get("date", "N/A")
             })
+
+markdown_files.sort(key=lambda x: x.get("title"), reverse=True)
 
 with open("_data/markdown_files.yml", "w") as outfile:
     yaml.dump(markdown_files, outfile, default_flow_style=False)
