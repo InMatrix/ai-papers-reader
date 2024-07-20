7f14b3a15c6e1a9c47249e0c5a4196e657673f55
Tao Dong <tomdong@gmail.com>
2024-07-20 15:31:51 -0700

    Update markdown_files.yml and list_md_files.py

--- src/list_md_files.py
+++ src/list_md_files.py
@@ -1,10 +1,9 @@
 import os
 import yaml
-import frontmatter
 
 # Format link title as "Report YYYY-MM-DD"
 def formatTitle(file_path):
-    parts = file_path[8:-3].split('_')
+    parts = file_path[5:-3].split('_')
     return ' '.join(word.capitalize() for word in parts)    
 
 markdown_files = []
@@ -12,10 +11,9 @@ for root, _, files in os.walk("docs"):
     for file in files:
         if file.endswith(".md") and not(file.startswith("index")):
             file_path = os.path.join(root, file)
-            # post = frontmatter.load(file_path)
             markdown_files.append({
                 "title": formatTitle(file_path),
-                "url": file_path[8:-3],  
+                "url": file_path[5:-3].replace("_", "-") 
             })
 
 markdown_files.sort(key=lambda x: x.get("title"), reverse=True)
